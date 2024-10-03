a5e1fd2a9805f11e2b23114f8d1fa784fee69aa2
PShinde630 <159200560+PShinde630@users.noreply.github.com>
2024-10-02 23:27:46 -0700

    Update main.py

--- QuoteService/main.py
+++ QuoteService/main.py
@@ -3,44 +3,43 @@ from flask import jsonify
 from pymongo import MongoClient
 import redis
 
-def get_db():
-    client = MongoClient(host='db',
+def getdb():
+    client = MongoClient(host='localhost',
                          port=27017,
-                         username='root',
-                         password='pass',
+                         username='admin',
+                         password='admin',
                          authSource="admin")
-    db = client["quote_db"]
+    db = client["quotedb"]
     return db
 
-def get_redis():
-    r = redis.Redis(host='redis',
+def getredis():
+    r = redis.Redis(host='127.0.0.1',
                     port=6379)
     return r
 
 
-# a quote
+a quote
 class Quote(object):
-    def __init__(self, quote, by):
+    def init(self, quote, by):
         self.quote = quote
         self.by = by
 
 # # Loads quotes from a file
-# def loadQuotes():
-#     with open(QUOTES_FILE) as file:
-#         lines = file.readlines()
-#         lines = [x.strip() for x in lines]
-#         for line in lines:
-#             quote, by = line.split("-")
-#             quotes.append(Quote(quote, by))
+def loadQuotes():
+with open(QUOTES_FILE) as file:
+lines = file.readlines()
+lines = [x.strip() for x in lines]
+for line in lines:
+quote, by = line.split("-")
+quotes.append(Quote(quote, by))
+app = Flask(name)
 
-app = Flask(__name__)
-
-# Gets a random quote
+Gets a random quote
 @app.route("/api/quote")
 def quote():
     db=""
     try:
-        db = get_db()
+        db = getdb()
         r = get_redis()
         count = r.get("count")
         if count is None:
@@ -63,10 +62,10 @@ def quote():
         if type(db)==MongoClient:
             db.close()
 
-# 404 Erorr for unknown routes
+404 Erorr for unknown routes
 @app.errorhandler(404)
 def page_not_found(e):
     return jsonify({"message": "Resource not found"}), 404
 
-if __name__ == '__main__':
+if __name == '__main':
     app.run(host='0.0.0.0', port=5000, debug=True) # run application
