a116df5d4e49f184a1b9c0b46637066c01596070
PShinde630 <159200560+PShinde630@users.noreply.github.com>
2024-10-03 00:11:49 -0700

    Update main.py

--- QuoteService/main.py
+++ QuoteService/main.py
@@ -3,43 +3,44 @@ from flask import jsonify
 from pymongo import MongoClient
 import redis
 
-def getdb():
+def get_db():
     client = MongoClient(host='localhost',
                          port=27017,
                          username='admin',
                          password='admin',
                          authSource="admin")
-    db = client["quotedb"]
+    db = client["quote_db"]
     return db
 
-def getredis():
+def get_redis():
     r = redis.Redis(host='127.0.0.1',
                     port=6379)
     return r
 
 
-a quote
+# a quote
 class Quote(object):
-    def init(self, quote, by):
+    def __init__(self, quote, by):
         self.quote = quote
         self.by = by
 
 # # Loads quotes from a file
-def loadQuotes():
-with open(QUOTES_FILE) as file:
-lines = file.readlines()
-lines = [x.strip() for x in lines]
-for line in lines:
-quote, by = line.split("-")
-quotes.append(Quote(quote, by))
-app = Flask(name)
+# def loadQuotes():
+#     with open(QUOTES_FILE) as file:
+#         lines = file.readlines()
+#         lines = [x.strip() for x in lines]
+#         for line in lines:
+#             quote, by = line.split("-")
+#             quotes.append(Quote(quote, by))
 
-Gets a random quote
+app = Flask(__name__)
+
+# Gets a random quote
 @app.route("/api/quote")
 def quote():
     db=""
     try:
-        db = getdb()
+        db = get_db()
         r = get_redis()
         count = r.get("count")
         if count is None:
@@ -62,10 +63,10 @@ def quote():
         if type(db)==MongoClient:
             db.close()
 
-404 Erorr for unknown routes
+# 404 Erorr for unknown routes
 @app.errorhandler(404)
 def page_not_found(e):
     return jsonify({"message": "Resource not found"}), 404
 
-if __name == '__main':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True) # run application
